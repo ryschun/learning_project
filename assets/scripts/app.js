9fcee93a3ee8600c82b350076727655993df7ca4
ryschun <volodymyr.ryshkovskyy@gmail.com>
2021-12-22 18:34:08 +0200

    Lets start functions

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${defaultResult} +   10)`;
+let errorMassage = 'an error \n \n' + 'occured' //ми юзаємо бек слеш щоб передати "особливий елемент який до стрінга не має відношення"
+// якщо ми пишемо \n тоді ми робимо переривання строки
+// let currentResult = 2
+// let calculationDescription = 2
+ outputResult(currentResult,errorMassage)
+
