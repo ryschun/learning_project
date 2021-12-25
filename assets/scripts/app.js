eb3b07f62120f149b9352e59b6f4efa955fc661f
ryschun <volodymyr.ryshkovskyy@gmail.com>
2021-12-25 17:18:27 +0200

    func

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,17 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+function add(num1,num2){
+    const result = num1+num2; 
+    return result;
+}
+
+currentResult = add(0,1);
+
 
 let calculationDescription = `(${defaultResult} +   10*3/2-1)`;
 
-let errorMassage = 'an error \n \n' + 'occured' //ми юзаємо бек слеш щоб передати "особливий елемент який до стрінга не має відношення"
+let errorMassage = 'an error \n \n' + 'occured'; //ми юзаємо бек слеш щоб передати "особливий елемент який до стрінга не має відношення"
 // якщо ми пишемо \n тоді ми робимо переривання строки
 // let currentResult = 2
 // let calculationDescription = 2
